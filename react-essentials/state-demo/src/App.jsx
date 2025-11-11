b598057e77e89cbfd1269ea01a4be179a05cb38c
Vishwas <vishwas.g5@gmail.com>
2025-11-11 11:18:56 +0100

    Boolean and string state

--- react-essentials/state-demo/src/App.jsx
+++ react-essentials/state-demo/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Counter } from "./Counter";
+import { LoginCard } from "./LoginCard";
 
 function App() {
   return (
     <>
       <Counter />
+      <LoginCard />
     </>
   );
 }
