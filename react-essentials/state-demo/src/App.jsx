ce19b6b3d12320bf207270a1fcb744b7c8f24051
Vishwas <vishwas.g5@gmail.com>
2025-11-12 11:31:56 +0100

    Rules of hooks

--- react-essentials/state-demo/src/App.jsx
+++ react-essentials/state-demo/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Counter } from "./Counter";
 import { LoginCard } from "./LoginCard";
+import { UserDashboard } from "./UserDashboard";
 
 function App() {
   return (
     <>
       <Counter />
       <LoginCard />
+      <UserDashboard isPremium={true} />
     </>
   );
 }
