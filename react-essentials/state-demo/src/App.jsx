57153d01063d79072fe4c5e94e5b40634425a36c
Vishwas <vishwas.g5@gmail.com>
2025-11-15 10:26:06 +0100

    State as a snapshot

--- react-essentials/state-demo/src/App.jsx
+++ react-essentials/state-demo/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Counter } from "./Counter";
 import { LoginCard } from "./LoginCard";
 import { UserDashboard } from "./UserDashboard";
+import { SimpleCounter } from "./SimpleCounter";
 
 function App() {
   return (
     <>
+      <SimpleCounter />
       <Counter />
       <LoginCard />
       <UserDashboard isPremium={true} />
